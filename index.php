3236b8ff30e1bf1f2df5e4b711693d67d7cad57e
Out of Ink Software <moshekrum@gmail.com>
2014-08-18 00:32:11 -0400

    added compressed vs regular json

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require 'vendor/autoload.php';
 
 $app = new \Slim\Slim();
-
+$boi_json = file_get_contents("boi.json");
 $app->get('/hello/:name', function ($name) {
     echo "Hello, $name!";
 });
@@ -10,8 +10,10 @@ $app->get('/hello/:name', function ($name) {
 $app->get('/', function () {
     echo "Hello!";
 });
-$app->get('/boi/allitems', function () {
-    $boi_json = file_get_contents("boi.json");
+$app->get('/boi/allitems/compressed', function () {
     echo $boi_json;
+});
+$app->get('/boi/allitems', function () {
+    echo nl2br($boi_json);
 });
 $app->run();
