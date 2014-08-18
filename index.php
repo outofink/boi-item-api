77fa09b688728c6fefdde6a72e36dfbff2415ab2
Out of Ink Software <moshekrum@gmail.com>
2014-08-18 13:02:46 -0400

    added basic id API functionality

--- index.php
+++ index.php
@@ -8,12 +8,8 @@ $boi_json = file_get_contents("boi.json");
 $boi = json_decode($boi_json, true);
 $boi_json_v2 = json_encode($boi, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
 
-$app->get('/hello/:name', function ($name) {
-    echo "Hello, $name!";
-});
-
 $app->get('/', function () {
-    echo "Hello!";
+    echo "Welcome to THe Binding of Isaac item API!";
 });
 $app->get('/boi/items/all/compressed', function () {
     global $boi_json_v2;
@@ -28,5 +24,15 @@ $app->get('/boi/items/all', function () {
 $app->get('/boi/items', function () use ($app) {
     $app->redirect('/boi/items/all');
 });
-
+$app->get('/boi/items/:id', function ($id) {
+    for ($i = 0; ; $i++) {
+        if ($boi["items"][$i]["itemid"] = $id) {
+            echo $boi["items"][$i];
+            break;
+        }
+        if ($i > 200) {
+            echo "Error, item not found.";
+            break;
+        }
+});
 $app->run();
