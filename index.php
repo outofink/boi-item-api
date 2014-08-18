5277f405d9800c7e0312ee6c02185d632161eed5
Out of Ink Software <moshekrum@gmail.com>
2014-08-18 00:35:04 -0400

    fixed global variables

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ require 'vendor/autoload.php';
 
 $app = new \Slim\Slim();
 $boi_json = file_get_contents("boi.json");
+
 $app->get('/hello/:name', function ($name) {
     echo "Hello, $name!";
 });
@@ -11,9 +12,12 @@ $app->get('/', function () {
     echo "Hello!";
 });
 $app->get('/boi/allitems/compressed', function () {
+    global $boi_json;
     echo $boi_json;
 });
 $app->get('/boi/allitems', function () {
+    global $boi_json;
     echo nl2br($boi_json);
 });
+
 $app->run();
