37da39c644b4e2402feac04613e4208f368978e6
Out of Ink Software <moshekrum@gmail.com>
2014-08-18 11:09:32 -0400

    working on api syntax

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $app = new \Slim\Slim();
 
 $boi_json = file_get_contents("boi.json");
 
-$boi = json_decode($boi_json);
+$boi = json_decode($boi_json, true);
 $boi_json_v2 = json_encode($boi, JSON_PRETTY_PRINT, JSON_UNESCAPED_SLASHES);
 
 $app->get('/hello/:name', function ($name) {
@@ -15,15 +15,18 @@ $app->get('/hello/:name', function ($name) {
 $app->get('/', function () {
     echo "Hello!";
 });
-$app->get('/boi/allitems/compressed', function () {
+$app->get('/boi/items/all/compressed', function () {
     global $boi_json;
     echo $boi_json;
 });
-$app->get('/boi/allitems', function () {
+$app->get('/boi/items/all', function () {
     global $boi_json_v2;
     echo "<pre>";
     echo $boi_json_v2;
     echo "</pre>";
+});
+$app->get('/boi/items', function () use ($app) {
+    $app->redirect('/boi/items/all');
 });
 
 $app->run();
