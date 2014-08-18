4fc1fc34a3ee9e3ae1472c3c5cd08fd3802c7e80
Out of Ink Software <moshekrum@gmail.com>
2014-08-18 15:03:30 -0400

    moved equals sign

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ $app->get('/boi/items', function () use ($app) {
 $app->get('/boi/items/:id', function ($id) {
     global $boi;
     $newid = intval($id)-1;
-    if ($newid > 0 and $newid <= 198) {
+    if ($newid >= 0 and $newid < 198) {
         $items = $boi["items"];
         $itembyid = json_encode($items[$newid], JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
         echo "<pre>";
