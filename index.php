b8050ea3594ad5c35307f165cbf629e73805dde1
Out of Ink Software <moshekrum@gmail.com>
2014-08-18 00:40:23 -0400

    attempt to fix layout

--- index.php
+++ index.php
@@ -2,7 +2,11 @@
 require 'vendor/autoload.php';
 
 $app = new \Slim\Slim();
-$boi_json = file_get_contents("boi.json");
+
+$boi_json = file_get_contents("boi.json")
+
+$boi = json_decode($boi_json);
+$boi_json_v2 = json_encode($boi, JSON_PRETTY_PRINT);
 
 $app->get('/hello/:name', function ($name) {
     echo "Hello, $name!";
@@ -16,8 +20,8 @@ $app->get('/boi/allitems/compressed', function () {
     echo $boi_json;
 });
 $app->get('/boi/allitems', function () {
-    global $boi_json;
-    echo nl2br($boi_json);
+    global $boi_json_v2;
+    echo nl2br($boi_json_v2);
 });
 
 $app->run();
